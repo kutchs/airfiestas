3d3bf4ecd9474e15f0105723cc11b35ba901a523
Arnaud Lothe <arnaud.lothe@gmail.com>
2024-06-13 21:48:37 +0200

    configuration de base des controlleurs

--- app/controllers/myevents_controller.rb
+++ app/controllers/myevents_controller.rb
@@ -1,4 +1,7 @@
 class MyeventsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @events = current_user.events
   end
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,37 @@
 class ReservationsController < ApplicationController
+  before_action :set_reservation, only: [:update, :validate]
+  before_action :authenticate_user!
+
+  def index
+    @reservations = current_user.reservations
+  end
+
   def new
+    @event = Event.find(params[:event_id])
+    @reservation = @event.reservations.build
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @reservation = @event.reservations.build(reservation_params)
+    @reservation.user = current_user
+    if @reservation.save
+      redirect_to reservations_path, notice: 'La réservation a été créée avec succès.'
+    else
+      render :new
+    end
   end
 
   def update
+    if @reservation.update(reservation_params)
+      redirect_to reservations_path, notice: 'La réservation a été mise à jour avec succès.'
+    else
+      render :edit
+    end
   end
 
   def validate
+    @reservation.update(status: "validated")
+    redirect_to reservations_path, notice: 'La réservation a été validée avec succès.'
   end
 end
