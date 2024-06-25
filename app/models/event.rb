ba71c2dea31b30de599358edce165604c6219506
Julien Lamouroux <lamourouxjulien+lewagon@icloud.com>
2024-06-25 16:57:35 +0200

    obligation de mettre certains champs lors de l'ajout d'un event + display d'un message d'erreur sinon

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
   has_one_attached :image
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :date_time, presence: true
 end
